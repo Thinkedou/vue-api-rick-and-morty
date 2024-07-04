eb65edf1fe65ac76b575c75f02ecb23f0bfabd76
Thinkedou <r.enguehard@gmail.com>
2024-07-04 14:41:54 +0200

    first

---
+++ src/App.vue
@@ -0,0 +1,109 @@
+<script setup>
+import {  RouterView } from 'vue-router'
+import ky from 'ky';
+
+
+const json =  ky.get('https://rickandmortyapi.com/api/character').json()
+
+const {results} = json 
+console.log(json)
+
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container px-4 px-lg-5">
+                <span class="navbar-brand" >Async / Await</span>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                        <li class="nav-item">
+                        	<a class="nav-link active" aria-current="page" href="#!">Home</a>
+                        </li>
+                        <li class="nav-item">
+                        	<a class="nav-link" href="#!">Liste</a>
+                        </li>
+                        
+                    </ul>
+                    
+                </div>
+            </div>
+  </nav>
+    <!-- Header-->
+    <header class="bg-dark py-5">
+        <div class="container px-4 px-lg-5 my-5">
+            <div class="text-center text-white">
+                <h1 class="display-4 fw-bolder"> ⚡ API CALLING ⚡</h1>
+                <p class="lead fw-normal text-white-50 mb-0"> With axios or fetch </p>
+            </div>
+        </div>
+    </header>
+      <RouterView />  
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2023</p></div>
+        </footer>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
